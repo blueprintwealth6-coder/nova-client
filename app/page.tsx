63e9fe702efe875f9545f4539372fcae7dea25ed
blueprintwealth6-coder <faizanshahid1541@gmail.com>
2026-07-08 13:35:50 +0500

    Fix Sidebar import

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,16 @@
-import SectionAnimation from "@/components/SectionAnimation";
-import FAQ from "@/components/FAQ";
-import CTA from "@/components/CTA";
-import Pricing from "@/components/Pricing";
-import Testimonials from "@/components/Testimonials";
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import Stats from "@/components/Stats";
-import Features from "@/components/Features";
-import AIShowcase from "@/components/AIShowcase";
-import Trending from "@/components/Trending";
-import VideoFeed from "@/components/VideoFeed";
-import CreatorCards from "@/components/CreatorCards";
-import Footer from "@/components/Footer";
-import BackgroundGlow from "@/components/BackgroundGlow";
+"use client";
+import Feed from "../components/Feed";
 
 export default function Home() {
   return (
-    <main className="bg-[#050816] text-white overflow-hidden relative">
-<SectionAnimation>
-      <BackgroundGlow />
-      </SectionAnimation>
-      <SectionAnimation>
-      <Navbar />
-      </SectionAnimation>
-      <SectionAnimation>
-      <Hero />
-      </SectionAnimation>
-      <SectionAnimation>
-      <Stats />
-      </SectionAnimation>
-      <SectionAnimation>
-      <Features />
-      </SectionAnimation>
-      <SectionAnimation>
-      <AIShowcase />
-      </SectionAnimation>
-      <SectionAnimation>
-      <Trending />
-      </SectionAnimation>
-      <SectionAnimation>
-      <VideoFeed />
-      </SectionAnimation>
-      <SectionAnimation>
-      <CreatorCards />
-      </SectionAnimation>
-      <SectionAnimation>
-      <Testimonials />
-      </SectionAnimation>
-      <SectionAnimation>
-      <Pricing />
-      </SectionAnimation>
-      <SectionAnimation>
-      <CTA />
-      </SectionAnimation>
-      <SectionAnimation>
-      <FAQ />
-      </SectionAnimation>
-      <SectionAnimation>
-      <Footer />
-      </SectionAnimation>
-    </main>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        paddingTop: "30px",
+      }}
+    >
+      <Feed />
+    </div>
   );
 }
