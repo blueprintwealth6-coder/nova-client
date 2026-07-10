f38194810ba6b01280520d79edbadbba383bc426
blueprintwealth6-coder <faizanshahid1541@gmail.com>
2026-07-10 14:47:18 +0500

    Added welcome page and feed route

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,64 +1,17 @@
-
 "use client";
 
-import { useState, useEffect } from "react";
-import API from "@/lib/api";
-import VideoCard from "@/components/VideoCard";
-import BottomNav from "@/components/BottomNav";
+import Feed from "../../components/Feed";
 
 export default function FeedPage() {
-  const [videos, setVideos] = useState<any[]>([]);
-
-  useEffect(() => {
-    loadFeed();
-  }, []);
-
-  const loadFeed = async () => {
-    try {
-      const res = await API.get("/video/feed");
-      setVideos(res.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   return (
     <div
       style={{
-        background: "#000",
-        width: "100%",
-        height: "100vh",
-        overflowY: "scroll",
-        scrollSnapType: "y mandatory",
         display: "flex",
         justifyContent: "center",
-        paddingRight: "110px",
+        paddingTop: "30px",
       }}
     >
-      <div
-        style={{
-          width: "100%",
-          maxWidth: "520px",
-        }}
-      >
-        {videos.map((video) => (
-          <div
-            key={video._id}
-            style={{
-              height: "100vh",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              scrollSnapAlign: "start",
-            }}
-          >
-            <VideoCard video={video} />
-          </div>
-        ))}
-      </div>
-
-      <BottomNav />
+      <Feed />
     </div>
   );
 }
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,55 @@
 "use client";
-import Feed from "../components/Feed";
+
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div
       style={{
+        height: "100vh",
+        background: "#000",
+        color: "#fff",
         display: "flex",
         justifyContent: "center",
-        paddingTop: "30px",
+        alignItems: "center",
+        flexDirection: "column",
       }}
     >
-      <Feed />
+      <h1
+        style={{
+          fontSize: "70px",
+          color: "#18b6ff",
+          marginBottom: "20px",
+        }}
+      >
+        NOVA
+      </h1>
+
+      <p
+        style={{
+          color: "#aaa",
+          fontSize: "22px",
+          marginBottom: "40px",
+        }}
+      >
+        Watch • Upload • Connect
+      </p>
+
+      <Link href="/feed">
+        <button
+          style={{
+            padding: "18px 45px",
+            fontSize: "20px",
+            border: "none",
+            borderRadius: "50px",
+            background: "#18b6ff",
+            color: "#fff",
+            cursor: "pointer",
+          }}
+        >
+          Enter NOVA
+        </button>
+      </Link>
     </div>
   );
 }
